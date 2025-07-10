5357a41f6e11900e1d06b052a53c381a77448182
i.alex <164629653+AlexCodeCraft@users.noreply.github.com>
2025-07-10 22:31:36 +0200

    Update secure_linux.sh

--- scripts/secure_linux.sh
+++ scripts/secure_linux.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+sudo apt update && sudo apt upgrade -y
+sudo apt install ufw fail2ban -y
+sudo ufw allow OpenSSH
+sudo ufw enable
+sudo sed -i 's/^PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config
+sudo systemctl restart ssh
+sudo systemctl enable fail2ban
+sudo systemctl start fail2ban
+echo "Sécurisation terminée !"
 
